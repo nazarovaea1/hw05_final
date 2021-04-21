9babb942a6478b0de50f5685492337018c4a9560
Назарова Екатерина <nazarovaea1@gmail.com>
2021-04-21 17:40:00 +0300

    Added double password confirmation in registration

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
@@ -5,6 +6,33 @@ User = get_user_model()
 
 
 class CreationForm(UserCreationForm):
+    error_messages = {
+        'password_mismatch': ("Пароли не совпадают."),
+    }
+    password1 = forms.CharField(label=("Пароль"), widget=forms.PasswordInput)
+    password2 = forms.CharField(label=("Подтвердите пароль"),
+                                widget=forms.PasswordInput,
+                                help_text=(
+                                    "Введите Ваш пароль еще раз, "
+                                    "для подтверждения."))
+
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("first_name", "last_name", "username", "email")
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
